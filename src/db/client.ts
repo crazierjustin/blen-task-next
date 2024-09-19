d45a3aeb7d6114ab1e508c673bc08ed30210e277
Justin Smith <justin12.14smith@gmail.com>
2024-09-18 21:14:30 -0400

    set up Drizzle ORM and SQLite

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,5 @@
-import { DB_NAME } from '@/constants/db';
 import Database from 'better-sqlite3';
-import { BetterSQLite3Database, drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 
-const tasksDb = new Database(DB_NAME);
-export const db: BetterSQLite3Database = drizzle(tasksDb);
+const sqlite = new Database('../../database.sqlite');
+const db = drizzle(sqlite);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,3 @@
-import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const tasks = sqliteTable('tasks', {
@@ -6,13 +5,6 @@ export const tasks = sqliteTable('tasks', {
   title: text('title').notNull(),
   description: text('description').notNull(),
   dueDate: text('due_date').notNull(),
-  isCompleted: integer('is_completed', { mode: 'boolean' }).notNull().default(false),
-  createdAt: text('created_at')
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: text('updated_at')
-    .notNull()
-    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
 });
 
 export type Task = typeof tasks.$inferSelect;
